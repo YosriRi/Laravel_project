ae61ccff4c1846cfab06a8807ce6d0e1852aa911
RiYosri <riani.yosri@gmail.com>
2018-07-11 14:02:31 +0200

    login register page

---
+++ resources/views/include/footer.blade.php
@@ -0,0 +1,143 @@
+
+<section id="contact" class="footer_widget">
+    <div class="container">
+        <div class="row">
+            <div class="main_widget">
+                <div class="col-md-4 col-12">
+                    <div class="single_widget wow fadeIn" data-wow-duration="800ms">
+                        <div class="footer_logo">
+                            <img src="images/footerlogo.png" alt="" />
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat. </p>
+
+
+                        <div class="footer_subcribs_area">
+                            <p class="monseratregular"><strong>15k</strong> followers</p>
+                            <hr />
+                            <div class="flowus">
+                                <a href="" class="robotolight"> Follow Us : </a>
+                                <a href=""><i class="fa fa-facebook"></i></a>
+                                <a href=""><i class="fa fa-twitter"></i></a>
+                                <a href=""><i class="fa fa-google-plus"></i></a>
+                                <a href=""><i class="fa fa-instagram"></i></a>
+                                <a href=""><i class="fa fa-pinterest-p"></i></a>
+                                <a href=""><i class="fa fa-youtube"></i></a>
+                                <a href=""><i class="fa fa-rss"></i></a>
+                                <a href=""><i class="fa fa-dribbble"></i></a>
+                            </div>
+
+                            <form class="navbar-form navbar-left" role="search">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                    <button type="submit" class="submit_btn">Subscribe</button>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-md-7  col-12">
+                            <div class="single_widget wow fadeIn" data-wow-duration="800ms">
+                                <p class="monseratregular">Dernières posts</p>
+                                <span class="separator4"></span>
+                                <div class="single_widget_blog">
+                                    <div class="single_widget_img">
+                                        <img src="images/fb1.jpg" alt="" />
+                                    </div>
+                                    <div class="single_widget_content">
+                                        <p class="monseratregular">Lorem ipsum dolor sit amet, 
+                                            consectetur adipiscing</p>
+                                        <p class="robotolight">Jan 9, 2016</p>
+                                    </div>
+                                </div>
+                                <div class="single_widget_blog">
+                                    <div class="single_widget_img">
+                                        <img src="images/fb2.jpg" alt="" />
+                                    </div>
+                                    <div class="single_widget_content">
+                                        <p class="monseratregular">Lorem ipsum dolor sit amet, 
+                                            consectetur adipiscing</p>
+                                        <p class="robotolight">Jan 9, 2018</p>
+                                    </div>
+                                </div>
+                                <div class="single_widget_blog">
+                                    <div class="single_widget_img">
+                                        <img src="images/fb3.jpg" alt="" />
+                                    </div>
+                                    <div class="single_widget_content">
+                                        <p class="monseratregular">Lorem ipsum dolor sit amet, 
+                                            consectetur adipiscing</p>
+                                        <p class="robotolight">Jan 9, 2018</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-5 col-12">
+                            <div class="single_widget wow fadeIn" data-wow-duration="800ms">
+                                <p class="monseratregular">instagram</p>
+                                <span class="separator4"></span>
+
+                                <div class="footer_gellary">
+                                    <div class="row">
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg1.jpg" alt="" />
+                                        </div>
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg2.jpg" alt="" />
+                                        </div>
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg3.jpg" alt="" />
+                                        </div>
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg4.jpg" alt="" />
+                                        </div>
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg5.jpg" alt="" />
+                                        </div>
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg6.jpg" alt="" />
+                                        </div>
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg7.jpg" alt="" />
+                                        </div>
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg8.jpg" alt="" />
+                                        </div>
+                                        <div class="col-md-4 col-6 no-padding">
+                                            <img src="images/fg9.jpg" alt="" />
+                                        </div>
+                                    </div> 
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<footer id="footer" class="footer">
+    <div class="container">
+        <div class="main_footer text-center">
+            <div class="row">
+                <div class="col-md-12 col-12">
+                    <div class="copyright_text">
+                        <p class=" wow fadeInRight" data-wow-duration="1s">Made with <i class="fa fa-heart"></i> by FuriousDucks 2018. All Rights Reserved</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
