67f4b86718eb22ac7f8a60bf194a68879ceae8fb
RiYosri <riani.yosri@gmail.com>
2018-07-19 23:52:54 +0200

    Reservation profile pics template

---
+++ resources/views/include/profile.blade.php
@@ -0,0 +1,235 @@
+<!-- Bootstrap CSS -->
+<!-- jQuery first, then Bootstrap JS. -->
+<!-- Nav tabs -->
+
+<ul class="nav nav-tabs" role="tablist">
+    <li class="nav-item">
+        <a class="nav-link active" href="#profile" role="tab" data-toggle="tab">Profile</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#images" role="tab" data-toggle="tab">Images</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#references" role="tab" data-toggle="tab">References</a>
+    </li>
+</ul>
+
+<!-- Tab panes -->
+<div class="tab-content">
+  <div class="container">
+      <!-- Profile -->
+      <div role="tabpanel" class="tab-pane fade fade" id="profile">
+        <div class="profile-company-content">
+          <div class="col-md-12">
+              <div class="tab-content">
+                  <div class="profile-main">
+                      <h3>About</h3>
+                      <div class="profile-in">
+                          <div class="media-left">
+                              <div class="img-profile">
+                                  <img src="img/ab1.jpg" alt="" class="media-object">
+                              </div>
+                          </div>
+                          <div class="media-body">
+                              <p>
+                                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, maxime, excepturi, mollitia, voluptatibus similique aliquidautem
+                                  laudantium sapiente ad enim ipsa modi labo rum accusantium deleniti neque architecto vitae.
+                                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea, nihil, dolores, culpa ullam
+                                  vero ipsum placeat accusamus nemo itate id molestiae consectetur quae pariatur repudi andae
+                                  vel ex quaerat nam iusto aliquid laborum quia adipisci aut ut imcati nisi deleniti tempore
+                                  maxime sequi fugit reiciendis libero quo. Rerum, assumenda. Lorem ipsum dolor sit amet, consectetur
+                                  adipisicing elit. Exercitationem, at nemo inventore temporibus corporis suscipit.
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="row">
+                      <div class="col-md-8">
+                          <div class="sidebar">
+                              <h5 class="main-title"></h5>
+
+                          </div>
+                          <div class="sidebar">
+                              <h5 class="main-title"></h5>
+
+                          </div>
+                      </div>
+                      <div class="col-md-4">
+                          <div class="sidebar">
+                              <h5 class="main-title"></h5>
+                              <div class="sidebar-information">
+                                  <ul class="single-category">
+                                      <li class="row">
+                                          <h6 class="title col-xs-6">Name</h6>
+                                          <span class="subtitle col-xs-6">Yosri Riani</span>
+                                      </li>
+                                  </ul>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </div>
+      <!-- Photos -->
+      <div role="tabpanel" class="tab-pane in active" id="images">
+          <div class="container">
+              <div class="row">
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                        <div class="view">
+                          <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(145).jpg" data-size="1600x1067">
+                            <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(145).jpg" alt="placeholder" class="img-fluid ">
+                          </a>
+                        </div>
+                        <div class="card-body card-body-cascade">
+                            <a class="robotolight" href="">
+                                <i class="fa fa-eye"></i>&nbsp;552</a>
+
+                            <a class="robotolight" href="">
+                                <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                        </div>
+                      </div>
+
+                  </div>
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                          <div class="view">
+                            <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(150).jpg" data-size="1600x1067" class="">
+                              <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(150).jpg" alt="placeholder" class="img-fluid" />
+                            </a>
+                          </div>
+                          <div class="card-body card-body-cascade">
+                              <a class="robotolight" href="">
+                                <i class="fa fa-eye"></i>&nbsp;552</a>
+                              <a class="robotolight" href="">
+                                <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                          </div>
+                      </div>
+                  </div>
+
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                          <div class="view">
+                              <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(152).jpg" data-size="1600x1067">
+                                  <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(152).jpg" alt="placeholder" class="img-fluid" />
+                              </a>
+                          </div>
+                          <div class="card-body card-body-cascade">
+                              <a class="robotolight" href="">
+                                  <i class="fa fa-eye"></i>&nbsp;552</a>
+                              <a class="robotolight" href="">
+                                  <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                          </div>
+                      </div>
+                  </div>
+
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                          <div class="view">
+                              <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(42).jpg" data-size="1600x1067">
+                                  <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(42).jpg" alt="placeholder" class="img-fluid" />
+                              </a>
+                          </div>
+                          <div class="card-body card-body-cascade">
+                              <a class="robotolight" href="">
+                                  <i class="fa fa-eye"></i>&nbsp;552</a>
+                              <a class="robotolight" href="">
+                                  <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                          <div class="view">
+                              <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(151).jpg" data-size="1600x1067">
+                                  <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(151).jpg" alt="placeholder" class="img-fluid" />
+                              </a>
+                          </div>
+                          <div class="card-body card-body-cascade">
+                              <a class="robotolight" href="">
+                                  <i class="fa fa-eye"></i>&nbsp;552</a>
+                              <a class="robotolight" href="">
+                                  <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                          <div class="view">
+                              <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(40).jpg" data-size="1600x1067">
+                                  <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(40).jpg" alt="placeholder" class="img-fluid" />
+                              </a>
+                          </div>
+                          <div class="card-body card-body-cascade">
+                              <a class="robotolight" href="">
+                                  <i class="fa fa-eye"></i>&nbsp;552</a>
+                              <a class="robotolight" href="">
+                                  <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                          <div class="view">
+                              <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(148).jpg" data-size="1600x1067">
+                                  <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(148).jpg" alt="placeholder" class="img-fluid" />
+                              </a>
+                          </div>
+                          <div class="card-body card-body-cascade">
+                              <a class="robotolight" href="">
+                                  <i class="fa fa-eye"></i>&nbsp;552</a>
+                              <a class="robotolight" href="">
+                                  <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                          <div class="view">
+                              <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(147).jpg" data-size="1600x1067">
+                                  <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(147).jpg" alt="placeholder" class="img-fluid" />
+                              </a>
+                          </div>
+                          <div class="card-body card-body-cascade">
+                              <a class="robotolight" href="">
+                                  <i class="fa fa-eye"></i>&nbsp;552</a>
+                              <a class="robotolight" href="">
+                                  <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                          </div>
+                      </div>
+                  </div>
+
+                  <div class="mb-4 col-lg-3 col-md-4 col-xs-6">
+                      <div class="card z-depth-1">
+                          <div class="view">
+                              <a href="https://mdbootstrap.com/img/Photos/Lightbox/Original/img%20(149).jpg" data-size="1600x1067">
+                                  <img src="https://mdbootstrap.com/img/Photos/Lightbox/Thumbnail/img%20(149).jpg" alt="placeholder" class="img-fluid" />
+                              </a>
+                          </div>
+                          <div class="card-body card-body-cascade">
+                              <a class="robotolight" href="">
+                                  <i class="fa fa-eye"></i>&nbsp;552</a>
+                              <a class="robotolight" href="">
+                                  <i class="far fa-comment-dots"></i>&nbsp;17</a>
+                          </div>
+                      </div>
+                  </div>
+
+
+              </div>
+          </div>
+      </div>
+      <!-- References -->
+      <div role="tabpanel" class="tab-pane fade" id="references">ccc</div>
+  </div>
+</div>
