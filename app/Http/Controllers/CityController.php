a4980e36332305bf483ed52d306d452aa54fe462
Stephane <linstephane42@gmail.com>
2018-09-30 16:28:09 +0200

    commit

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -9,5 +9,78 @@ use App\Http\Resources\City as CityResource;
 
 class CityController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $cities = City::paginate(15);
+        
+        return CityResource::collection($cities);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $city = new City;
+
+        $city->name = $request->input('name');
+
+        if ($city->save()) {
+            return new CityResource($city);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $city = City::findOrFail($id);
+
+        return new CityResource($city);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $city = City::findOrFail($id);
+
+        $city->name = $request->input('name');
+
+        if ($city->save()) {
+            return new CityResource($city);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $city = City::findOrFail($id);
+
+        if ($city->delete()) {
+            return new CityResource($city);
+        }
+    }
 }
